542560d8d725f3509787e3799763182be03b6a88
Praveenrtp <46226861+Praveenrtp@users.noreply.github.com>
2022-12-05 19:26:07 -0500

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar1';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
 import About from './pages/about';
@@ -9,6 +10,13 @@ import AnnualReport from './pages/annual';
 import Teams from './pages/team';
 import Blogs from './pages/blogs';
 import SignUp from './pages/signup';
+
+
+import Dashboard from './pages/Dashboard';
+import Analytics from './pages/Analytics';
+import Comment from './pages/Comment';
+import Product from './pages/Product';
+import ProductList from './pages/ProductList';
 
 function App() {
 return (
@@ -23,6 +31,17 @@ return (
 		<Route path='/blogs' element={<Blogs />}></Route>
 		<Route path='/sign-up' element={<SignUp />}></Route>
 	</Routes>
+
+	<Sidebar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/comment" element={<Comment />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/productList" element={<ProductList />} />
+        </Routes>
 	</Router>
 );
 }
