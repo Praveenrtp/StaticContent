3a9d4fb9d7a89646af687685d14a3830bae799ca
Praveenrtp <46226861+Praveenrtp@users.noreply.github.com>
2022-12-05 21:47:23 -0500

    side bar and navbar done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar1';
+import Sidebar from './components/Sidebar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
 import About from './pages/about';
@@ -21,27 +21,26 @@ import ProductList from './pages/ProductList';
 function App() {
 return (
 	<Router>
-	<Navbar />
-	<Routes>
-    <Route path="/" element={<Home />}></Route>
-		<Route path='/about' element={<About />}></Route>
+		<Navbar />
+		<Sidebar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/comment" element={<Comment />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/product" element={<Product />} />
+		  <Route path="/dashboard" element={<Dashboard />} />
 		<Route path='/events' element={<Events />}></Route>
 		<Route path='/annual' element={<AnnualReport />}></Route>
 		<Route path='/team' element={<Teams />}></Route>
 		<Route path='/blogs' element={<Blogs />}></Route>
 		<Route path='/sign-up' element={<SignUp />}></Route>
-	</Routes>
-
-	<Sidebar />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/comment" element={<Comment />} />
-          <Route path="/analytics" element={<Analytics />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/productList" element={<ProductList />} />
+		<Route path='/ProductList' element={<ProductList />}></Route>
         </Routes>
+		</Sidebar>
+
+
+	
 	</Router>
 );
 }
